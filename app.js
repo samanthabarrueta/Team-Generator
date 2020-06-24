a0be7c7876437e390bf6def5fd2014736b1d7234
Samantha <samanthabarrueta@gmail.com>
2020-06-23 23:03:53 -0500

    inquirer questions /answers create general employee

--- app.js
+++ app.js
@@ -1,22 +1,76 @@
-const Manager = require("Manager");
-const Engineer = require("Engineer");
-const Intern = require("Intern");
+// const Manager = require("Manager");
+// const Engineer = require("Engineer");
+// const Intern = require("Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("htmlRenderer");
+// const render = require("htmlRenderer");
 
+const employeeInfo = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the employee\'s name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employee\'s I.D number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employee\'s email address?'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'What is the employee\'s role in the company?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'What is the manager\'s office number?',
+            when: function(answers) {
+                return answers.role ==="Manager";
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is this engineers\'s github username?',
+            when: function(answers) {
+                return answers.role ==="Engineer";
+            }
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Where did this intern go to school?',
+            when: function(answers) {
+                return answers.role ==="Intern";
+            }
+        }
+    ])
+    .then((answer) => {
+        const employee = {
+            name: answer.name,
+            email: answer.email,
+            role: answer.role
+        }
+        console.log(employee)
+    })
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+employeeInfo();
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -27,9 +81,3 @@ const render = require("htmlRenderer");
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
