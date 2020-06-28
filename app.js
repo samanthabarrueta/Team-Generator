27e94a395cd83cedd4ba78c481cb37de2400c282
Samantha <samanthabarrueta@gmail.com>
2020-06-28 09:27:40 -0500

    clean up code

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
 const path = require('path');
 const fs = require('fs');
 
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
-
 const render = require('./lib/htmlRenderer');
+
 const employees = [];
 
 const generateEmployees = () => {
@@ -80,17 +80,10 @@ const generateEmployees = () => {
         } else if (answers.continue === 'no') {
             fs.writeFile(outputPath, render(employees), (err) => {
                 if (err) throw err;
-                console.log('Saved!');
+                console.log('Your team page has been generated.');
             });
         }
     });
 }
 
 generateEmployees();
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
